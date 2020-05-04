e31eeaa169cac1d63e046c2d0867aab1c45e0fce
JarateKing <scmacintosh@upei.ca>
2020-05-04 08:21:27 -0300

    added comments for token

--- src/private_token_example.py
+++ src/private_token_example.py
@@ -1,1 +1,3 @@
+# create a file named "private_token.py" that is a copy of this, and then
+# replace this with your token from the 'bot' section in your application
 token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
